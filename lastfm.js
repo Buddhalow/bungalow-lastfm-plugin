2e9bc3ea43a5710e7b4fcaf1d95d98f839c5f491
Dr. Sounds <management@dr-sounds.com>
2017-12-03 13:01:26 +0000

    First commit

---
+++ lastfm.js
@@ -0,0 +1,11 @@
+define([
+], function (
+) {
+   document.addEventListener('mainmenuload', (e) => {
+        
+   });
+    document.addEventListener('viewstackloaded', () => {
+    
+        
+    });
+});
