c907f27d6ee657ef1e1bab3cd5ad4107a9c55d04
Dr. Sounds <management@dr-sounds.com>
2017-12-06 17:22:39 +0000

    Working on generalizing

--- lastfm.js
+++ lastfm.js
@@ -8,4 +8,19 @@ define([
     
         
     });
+    document.addEventListener('hook_searchview', (e) => {
+      
+    })
+    document.addEventListener('viewload', (e) => {
+        if (/^bungalow:artist:(.*)$/g.test(e.detail.uri)) {
+            var tab = e.detail.addTab('lastfm-biography', '<i class="fa fa-lastfm"></i> Biography');
+            
+            tab.style.display = 'relative';
+            $.getJSON('/api/lastfm/artist/' + encodeURI(e.detail.state.name), function (result) {
+                console.log(result);
+                tab.innerHTML = '<div class="plate">' + result.bio.description + '</div>';
+            });
+            
+        }
+    })
 });
