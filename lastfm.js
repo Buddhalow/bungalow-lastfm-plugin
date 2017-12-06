1f32f384943363ec35ca9de79d8f73806b4dd76e
Dr. Sounds <management@dr-sounds.com>
2017-12-06 21:17:24 +0000

    Commit

--- lastfm.js
+++ lastfm.js
@@ -14,11 +14,18 @@ define([
     document.addEventListener('viewload', (e) => {
         if (/^bungalow:artist:(.*)$/g.test(e.detail.uri)) {
             var tab = e.detail.addTab('lastfm-biography', '<i class="fa fa-lastfm"></i> Biography');
-            
-            tab.style.display = 'relative';
+            var container = document.createElement('div');
+            container.style.display = 'flex';
+            container.style.justifyContent = 'center';
+            container.style.alignItems = 'center';
+            container.style.width = '100%';
+            container.style.height = '100%';
+            tab.appendChild(container);
+            tab.style.alignItems = 'center';
+            tab.style.justifyContent = 'center';
             $.getJSON('/api/lastfm/artist/' + encodeURI(e.detail.state.name), function (result) {
                 console.log(result);
-                tab.innerHTML = '<div class="plate">' + result.bio.description + '</div>';
+                container.innerHTML = '<div class="plate">' + result.bio.description + '</div>';
             });
             
         }
